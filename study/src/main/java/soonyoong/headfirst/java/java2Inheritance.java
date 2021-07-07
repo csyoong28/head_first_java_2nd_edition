c4896b2ea89bf68e49834c24403faf287d7373d5
soonyoong.chia <soonyoong.chia@wdti.io>
2021-07-07 23:08:02 +0800

    more polymorphism

--- study/src/main/java/soonyoong/headfirst/java/java2Inheritance.java
+++ study/src/main/java/soonyoong/headfirst/java/java2Inheritance.java
@@ -3,12 +3,16 @@ package soonyoong.headfirst.java;
 public class java2Inheritance {
 	public static void main(String[] args) {
 		java2Inheritance java2Inheritance = new java2Inheritance();
-		
+		java2Inheritance.testPolymorphism();
 	}
 	
+	@SuppressWarnings("unused")
 	public void testPolymorphism() {
 		Animal animal2 = new Animal();   //Animal is abstract class, cannot be instantiated
 		Animal animal = new Dog();
-		animal.bark();	//cannot use mehtod on subclass, can use only those in superclass
+		//animal.bark();	//cannot use mehtod on subclass, can use only those in superclass
+		
+		Dog dog = new Animal();		//even if animal is concrete class, this will have compile error. also cannot cast animal to dog
+		dog.bark();
 	}
 }
